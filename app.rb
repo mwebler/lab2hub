0269680f3b31692f5a9d63d7c0524aba505ce85b
Matheus Webler <mwebler@picniic.com>
2018-07-24 23:14:55 -0700

    added call to create issue and comments on github from gitlab

--- app.rb
+++ app.rb
@@ -6,31 +6,26 @@ hub = Octokit::Client.new(access_token: ENV["GITHUB_TOKEN"])
 lab = Gitlab.client(endpoint: "https://gitlab.com/api/v4", private_token: ENV["GITLAB_TOKEN"])
 
 GITLAB_REPO = "mwebler/issues"
-
 GITLAB_MAX_PAGINATION=100
+
+GITHUB_REPO = "mwebler/issues"
 
 issues = lab.issues(GITLAB_REPO, {per_page: GITLAB_MAX_PAGINATION})
 
 issues.auto_paginate do |issue|
-    # hub.create_issue("mwebler/issues", issue.title, nil, {labels: "bug, todo, imported"})
-    puts("#{issue.id} - #{issue.title}")
-    puts("\n")
-    puts(issue.labels)
-    puts("\n")
-    puts(issue.state)
-    puts("\n")
-    puts(issue.description)
-    puts("\n")
+    github_issue = hub.create_issue(
+        GITHUB_REPO, issue.title, issue.description,
+        {labels: issue.labels.join(",")})
 
     comments = lab.issue_notes(issue.project_id, issue.iid, {per_page: GITLAB_MAX_PAGINATION})
     comments.auto_paginate do |comment|
         next if comment.system
 
-        puts("--- comment #{comment.id} ---\n")
-        puts(comment.body)
-        puts("\n")
+        hub.add_comment(GITHUB_REPO, github_issue.number, comment.body)
     end
 
-    puts("\n")
-    puts("\n")
+    if issue.state == "closed"
+        puts("closing issue #{github_issue.number}")
+        hub.close_issue(GITHUB_REPO, github_issue.number)
+    end
 end
