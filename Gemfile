7d75fe96fe836f28f3f71f42338b9120e1b29b3a
Matheus Webler <mwebler@picniic.com>
2018-07-27 02:30:58 -0700

    added simple web application authenticating with oauth2 on gitlab and saving issues to a test repo on github

--- Gemfile
+++ Gemfile
@@ -14,3 +14,9 @@ gem "rubocop", "~> 0.58.2"
 
 # Added at 2018-07-23 21:48:09 -0700 by matheus:
 gem "rubocop-github", "~> 0.10.0"
+
+# Added at 2018-07-25 00:51:42 -0700 by matheus:
+gem "sinatra", "~> 2.0"
+
+# Added at 2018-07-25 01:02:19 -0700 by matheus:
+gem "rest-client", "~> 2.0"
--- app.rb
+++ app.rb
@@ -1,46 +1,95 @@
 require "octokit"
 require "gitlab"
+require "sinatra"
+require "rest-client"
+require "json"
 
-hub = Octokit::Client.new(access_token: ENV["GITHUB_TOKEN"])
-
-lab = Gitlab.client(endpoint: "https://gitlab.com/api/v4", private_token: ENV["GITLAB_TOKEN"])
-
-GITLAB_REPO = "mwebler/issues"
 GITLAB_MAX_PAGINATION=100
 
-GITHUB_REPO = "mwebler/issues"
+use Rack::Session::Cookie, {key: "rack.session",
+                            expire_after: 1800, # after 30 minutes
+                            secret: ENV["SESSION_SECRET"],
+                            httponly: true }
 
-issues = lab.issues(GITLAB_REPO, {per_page: GITLAB_MAX_PAGINATION})
+def gitlab_to_github(gitlab_token, gitlab_repo, github_token, github_repo)
+    gitlab = Gitlab.client(endpoint: "https://gitlab.com/api/v4", private_token: gitlab_token)
+    github = Octokit::Client.new(access_token: github_token)
 
-issues_list = []
+    issues = gitlab.issues(gitlab_repo, {per_page: GITLAB_MAX_PAGINATION})
 
-issues.auto_paginate do |issue|
-    issue_to_copy = {
-        title: issue.title,
-        description: issue.description,
-        labels: issue.labels.join(","),
-        comments: [],
-        isClosed: issue.state == "closed"
-    }
+    issues_list = []
 
-    comments = lab.issue_notes(issue.project_id, issue.iid, {per_page: GITLAB_MAX_PAGINATION})
-    comments.auto_paginate do |comment|
-        # ignore system notes (i.e. label added, closed/reopened)
-        next if comment.system
+    issues.auto_paginate do |issue|
+        issue_to_copy = {
+            title: issue.title,
+            description: issue.description,
+            labels: issue.labels.join(","),
+            comments: [],
+            isClosed: issue.state == "closed"
+        }
 
-        issue_to_copy[:comments].unshift(comment.body)
+        comments = gitlab.issue_notes(issue.project_id, issue.iid, {per_page: GITLAB_MAX_PAGINATION})
+        comments.auto_paginate do |comment|
+            # ignore system notes (i.e. label added, closed/reopened)
+            next if comment.system
+
+            issue_to_copy[:comments].unshift(comment.body)
+        end
+
+        issues_list.unshift(issue_to_copy)
     end
 
-    issues_list.unshift(issue_to_copy)
+    issues_list.each do |issue|
+
+        github_issue = github.create_issue(
+            github_repo, issue[:title], issue[:description],
+            {labels: issue[:labels]})
+
+        issue[:comments].each { |comment| github.add_comment(github_repo, github_issue.number, comment) }
+
+        github.close_issue(github_repo, github_issue.number) if issue[:isClosed]
+    end
 end
 
-issues_list.each do |issue|
+get "/" do
+    user_data = {}
+    if session[:gitlab_token]
+        lab = Gitlab.client(endpoint: "https://gitlab.com/api/v4", private_token: session[:gitlab_token])
+        user_data[:gitlab_user] = lab.user.username
+        user_data[:gitlab_repos] = []
+        lab.projects({membership: true}).auto_paginate do |repo|
+            user_data[:gitlab_repos].push({
+                id: repo.id,
+                name: repo.path_with_namespace
+            })
+        end
+    end
+    if session[:github_token]
+        hub = Octokit::Client.new(access_token: ENV["GITHUB_TOKEN"])
+        user_data[:github_user] = hub.user.username
+    end
+    github_authenticated = true if session[:github_refresh_token]
+    erb :index, {locals: {user_data: user_data} }
+end
 
-    github_issue = hub.create_issue(
-        GITHUB_REPO, issue[:title], issue[:description],
-        {labels: issue[:labels]})
+get "/auth/gitlab" do
+    redirect to("https://gitlab.com/oauth/authorize?client_id=#{ENV["GITLAB_APP_ID"]}&redirect_uri=http://localhost:4567/authorized-gitlab&response_type=code&state=MYSTATE")
+end
 
-    issue[:comments].each { |comment| hub.add_comment(GITHUB_REPO, github_issue.number, comment) }
+get "/authorized-gitlab" do
+    parameters = "client_id=#{ENV["GITLAB_APP_ID"]}&client_secret=#{ENV["GITLAB_APP_SECRET"]}&code=#{params['code']}&grant_type=authorization_code&redirect_uri=http://localhost:4567/authorized-gitlab"
+    result = RestClient.post("https://gitlab.com/oauth/token", parameters)
+    credentials = JSON.parse(result.body)
+    session[:gitlab_token] = credentials["access_token"]
+    redirect to("/")
+end
 
-    hub.close_issue(GITHUB_REPO, github_issue.number) if issue[:isClosed]
+post "/copy" do
+    gitlab_token = session[:gitlab_token]
+    github_token = ENV["GITHUB_TOKEN"]
+
+    gitlab_repo = params['gitlab_repo'].to_i
+    github_repo = "mwebler/issues"
+    gitlab_to_github(gitlab_token, gitlab_repo, github_token, github_repo)
+    "done"
 end
