bb1c90635d87862cefe144c417149cb876dba5a3
Matheus Webler <mwebler@picniic.com>
2018-07-31 01:04:28 -0700

    updated github_client spec test to stub octokit instead of http calls. Added issue creation tests

--- spec/helpers/github_client_spec.rb
+++ spec/helpers/github_client_spec.rb
@@ -4,12 +4,9 @@ require "ostruct"
 require_relative "../../helpers/github_client"
 
 describe GithubClient do
-    before(:all) do
-        @github = GithubClient.new("testtoken")
-    end
-
     describe ".get_username" do
         before do
+            @github = GithubClient.new("testtoken")
             allow_any_instance_of(Octokit::Client).to receive(:user).and_return(
                 OpenStruct.new({login: "userhandle", id: 1, name: "User Test", email: "user@test.com" })
             )
@@ -22,6 +19,7 @@ describe GithubClient do
     describe ".get_repositories" do
         context "with 2 repositories" do
             before do
+                @github = GithubClient.new("testtoken")
                 allow_any_instance_of(Octokit::Client).to receive(:repos).and_return(
                     [
                         OpenStruct.new({id: 9999, full_name: "userhandle/repo"}),
@@ -43,6 +41,8 @@ describe GithubClient do
 
         context "with no repositories" do
             before do
+                @github = GithubClient.new("testtoken")
+
                 allow_any_instance_of(Octokit::Client).to receive(:repos).and_return([])
 
                 @repos = @github.get_repositories
@@ -53,4 +53,67 @@ describe GithubClient do
             end
         end
     end
+
+    describe ".create_issue_on_repository" do
+        context "create open issue without comments" do
+            before do
+                expect_any_instance_of(Octokit::Client).to receive(:create_issue)
+                .with("repo", "issue title", "issue body", {labels: ""})
+                .and_return(
+                    OpenStruct.new({number: 1234})
+                )
+                @github = GithubClient.new("testtoken")
+            end
+
+            it "succesfully creates an issue" do
+                new_issue = @github.create_issue_on_repository(
+                    "repo", {title: "issue title", description: "issue body", isClosed: false, comments: [], labels: []})
+                expect(new_issue.number).to be(1234)
+            end
+        end
+
+        context "create open issue with 2 comments" do
+            before do
+                expect_any_instance_of(Octokit::Client).to receive(:create_issue)
+                    .with("repo name", "another issue", "issue text", {labels: ""})
+                    .and_return(
+                        OpenStruct.new({number: 111})
+                    )
+
+                expect_any_instance_of(Octokit::Client).to receive(:add_comment)
+                    .with("repo name", 111, "My first comment")
+
+                expect_any_instance_of(Octokit::Client).to receive(:add_comment)
+                    .with("repo name", 111, "Just another comment")
+
+                @github = GithubClient.new("testtoken")
+            end
+
+            it "succesfully create two comments in an issue" do
+                @github.create_issue_on_repository(
+                    "repo name", {title: "another issue", description: "issue text", isClosed: false, comments: ["My first comment", "Just another comment"], labels: []})
+            end
+        end
+
+        context "create and close an issue" do
+            before do
+                expect_any_instance_of(Octokit::Client).to receive(:create_issue)
+                    .with("repository", "closed issue", "body", {labels: ""})
+                    .and_return(
+                        OpenStruct.new({number: 111})
+                    )
+
+                expect_any_instance_of(Octokit::Client).to receive(:close_issue)
+                    .with("repository", 111)
+
+                @github = GithubClient.new("testtoken")
+            end
+
+            it "succesfully close an issue" do
+                @github.create_issue_on_repository(
+                    "repository", {title: "closed issue", description: "body", isClosed: true, comments: [], labels: []})
+            end
+        end
+    end
+
 end
