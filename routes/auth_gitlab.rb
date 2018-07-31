6e3552611a95c74a648844fd6fba668653979d55
Matheus Webler <mwebler@picniic.com>
2018-07-31 03:33:07 -0700

    fixed app url env

--- routes/auth_gitlab.rb
+++ routes/auth_gitlab.rb
@@ -1,7 +1,7 @@
 require "rest-client"
 
 class MyApp < Sinatra::Base
-    @@REDIRECT_URI="#{ENV["APP_URL"].split(".").first}/auth/gitlab/callback&response_type=code&state=MYSTATE"
+    @@REDIRECT_URI="#{ENV["APP_URL"]}/auth/gitlab/callback&response_type=code&state=MYSTATE"
 
     # Redirect user to Gitlab OAuth authorization page
     get "/auth/gitlab" do
